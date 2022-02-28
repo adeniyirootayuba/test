9efdc3efb26cfc6f665e818cad867185b11224b1
Ayuba Adeniyi <ayuba.adeniyi@accenture.com>
2022-02-28 12:32:36 -0600

    first commit 5

--- csvtest.py
+++ csvtest.py
@@ -2,27 +2,27 @@ import os
 import csv
 import sys
 
-def compare_common_files_by_lines(directory_one, directory_two):
-   d1_files = set(os.listdir(directory_one))
-   d2_files = set(os.listdir(directory_two))
+def compare_common_files_by_lines(dir1, dir2):
+   csv_file1 = set(os.listdir(dir1))
+   csv_file2 = set(os.listdir(dir2))
    
-   if d1_files != d2_files:
+   if csv_file1 != csv_file2:
       print("Files name are not the samee")
       sys.exit(1)
    else:
-      common_files = list(d1_files & d2_files)
+      common_files = list(csv_file1 & csv_file2)
       if common_files:
         for filename in common_files:
            if filename.endswith('.csv'):
-             file_01 = open(f'{directory_one}/{filename}', 'r', encoding='ISO-8859-1')
-             file_02 = open(f'{directory_two}/{filename}', 'r', encoding='ISO-8859-1')
-             csv_file_01 = set(map(tuple, csv.reader(file_01)))
-             csv_file_02 = set(map(tuple, csv.reader(file_02)))
-             different = csv_file_01 ^ csv_file_02
+             file_01 = open(f'{dir1}/{filename}', 'r', encoding='ISO-8859-1')
+             file_02 = open(f'{dir2}/{filename}', 'r', encoding='ISO-8859-1')
+             csv_1 = set(map(tuple, csv.reader(file_01)))
+             csv_2 = set(map(tuple, csv.reader(file_02)))
+             different = csv_1 ^ csv_2
              for row in sorted(different, key=lambda x: x, reverse=True):
                 if row:
                      print(f'This row: \n {row} \n was different between the file {filename} in the directories'
-                             f' {directory_one} and {directory_two}')
+                             f' {dir1} and {dir2}')
       
 
 dir1 = '/home/ayuba/newfolder'
