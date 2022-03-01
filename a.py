d026012f30c4b3fd22f31bd2ffd41d7fc6d85bc5
Ayuba Adeniyi <ayuba.adeniyi@accenture.com>
2022-03-01 15:48:35 -0600

    first commit 5

--- a.py
+++ a.py
@@ -1,7 +1,24 @@
+from os import listdir
 import os
+
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    for filename in filenames:
+	if filename.endswith(suffix):
+           return str(filename)
+
+
+    
+#return [ filename for filename in filenames if filename.endswith( suffix ) ]
 
 
 dir = '/home/ayuba/newfolder'
-check = os.path.exists(os.path.join(os.getcwd(), dir, 'test1.csv'))
-print(check)
-print(os.path.join(os.getcwd(), dir, 'test1.csv'))
+dir2 = '/home/ayuba/newfolder2'
+
+check = os.path.isdir(dir)
+
+if check == True and find_csv_filenames(dir) == find_csv_filenames(dir2):
+	print("its the same "+ find_csv_filenames(dir) + " and " + find_csv_filenames(dir2))
+else:
+	print("No match " + find_csv_filenames(dir) + " and " + find_csv_filenames(dir2))
+
